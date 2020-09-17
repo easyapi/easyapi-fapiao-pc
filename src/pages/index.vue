a7b5350e203260f3e776c7c382069f2197d1d108
jyoyyz <jyoyyz@163.com>
2020-09-17 18:13:37 +0800

    chore:首页样式微改

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -49,7 +49,7 @@
             </a>
           </div>
           <div class="flex-r" v-if="!showAddressInfo">
-            <span class="modify-style">您还没有填写邮寄信息</span>
+            <span>您还没有填写邮寄信息</span>
             <a class=" flex-r modify-style" @click="jumpPage('/address')">现在填写
               <Icon size="24" type="md-create"/>
             </a>
@@ -60,13 +60,13 @@
         <div class="search-data flex-r">
           <div>
             <span class="area">起止时间</span>
-            <DatePicker type="datetimerange" placeholder="选择日期" @on-change="timeRangeChange" style="width: 340px"></DatePicker>
+            <DatePicker type="datetimerange" placeholder="选择日期" @on-change="timeRangeChange" style="width: 340px; margin-right: 4px"></DatePicker>
             <!-- <DatePicker @on-change="startTimeChange" type="datetime" placeholder="起始时间"
                         style="width: 180px;margin-right: 5px"></DatePicker>
             <span>-</span>
             <DatePicker @on-change="endTimeChange" type="datetime" placeholder="结束时间" style="width: 180px;margin-left: 5px"></DatePicker> -->
           </div>
-          <Select clearable v-model="state" style="width:190px" class="left-10" placeholder="请选择发票状态...">
+          <Select clearable v-model="state" style="width:190px; margin-right:4px" class="left-10" placeholder="请选择发票状态...">
             <Option v-for="item in stateList" :value="item" :key="item">{{ item}}</Option>
           </Select>
           <Input clearable v-model="purchaserName" placeholder="请输入发票抬头..." style="width: 190px" class="left-10"/>
@@ -212,7 +212,7 @@
         ],
         tableData: [],
         current: 1,
-        pageSize: 5,
+        pageSize: 10,
         total: 0,
       }
     },
@@ -442,6 +442,6 @@
   }
 
   .area{
-    margin-right: 10px;
+    margin-right: 12px;
   }
 </style>
