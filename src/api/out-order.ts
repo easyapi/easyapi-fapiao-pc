f1ef6e6d73e85541e7dfe7d81b9e4c177d928205
leida <zhang20084@126.com>
2023-07-23 12:34:28 +0800

    fix:获取外部订单数量与总金额

--- src/api/out-order.ts
+++ src/api/out-order.ts
@@ -41,3 +41,17 @@ export async function getOutOrderIdsApi(params): Promise<any> {
     },
   })
 }
+
+/**
+ * 获取外部订单数量与总金额
+ *
+ * @see https://www.easyapi.com
+ */
+export async function amountApi(params): Promise<any> {
+  return request.get('/out-order/amount', {
+    params: {
+      ...params,
+      accessToken: localStorage.get('accessToken'),
+    },
+  })
+}
